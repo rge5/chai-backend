66970a09f8e50474ea4317b77b4969fb1002407b
Ayush Shaw <ayushshaw590@gmail.com>
2026-09-24 00:49:39 +0530

    middleware update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -26,7 +26,7 @@ const userSchema = new Schema(
         coverImage: {
             type: String
         },
-        wathHiistory: [
+        watchHiistory: [
             {
                 type: Schema.Types.ObjectId,
                 ref:"Video"
@@ -47,7 +47,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
